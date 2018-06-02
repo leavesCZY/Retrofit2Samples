774a7152e9425e8b009d8875ad1094553032f75b
leavesC <199072443@qq.com>
2018-06-03 00:14:09 +0800

    download file

--- src/main/java/DownloadServiceMain.java
+++ src/main/java/DownloadServiceMain.java
@@ -1,3 +1,17 @@
+import client.DownloadService;
+import client.base.HttpConfig;
+import okhttp3.ResponseBody;
+import okio.BufferedSink;
+import okio.Okio;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.io.File;
+import java.io.IOException;
+
 /**
  * 作者：chenZY
  * 时间：2018/5/30 13:54
@@ -6,7 +20,47 @@
 public class DownloadServiceMain {
 
     public static void main(String[] args) {
+        downloadFile();
+    }
 
+    private static void downloadFile() {
+        DownloadService downloadService = buildRetrofit().create(DownloadService.class);
+        Call<ResponseBody> call = downloadService.downloadFile("downloadFile");
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if (response.isSuccessful()) {
+                    BufferedSink sink = null;
+                    try {
+                        File file = new File("C:\\Users\\CZY\\Desktop\\Hi.jpg");
+                        sink = Okio.buffer(Okio.sink(file));
+                        sink.writeAll(response.body().source());
+                        System.out.println("onResponse : success");
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    } finally {
+                        try {
+                            if (sink != null) {
+                                sink.close();
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                } else {
+                    System.out.println("onResponse code: " + response.code());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                System.out.println("onFailure: " + t.getMessage());
+            }
+        });
+    }
+
+    private static Retrofit buildRetrofit() {
+        return new Retrofit.Builder().baseUrl(HttpConfig.BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
     }
 
 }
--- src/main/java/client/DownloadService.java
+++ src/main/java/client/DownloadService.java
@@ -1,9 +1,20 @@
 package client;
 
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Streaming;
+import retrofit2.http.Url;
+
 /**
  * 作者：chenZY
  * 时间：2018/5/30 13:54
  * 描述：
  */
 public interface DownloadService {
+
+    @Streaming
+    @GET
+    Call<ResponseBody> downloadFile(@Url String fileUrl);
+
 }
